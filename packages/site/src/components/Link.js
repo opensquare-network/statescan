562f982677f036ef10668140fed46d3e1130e7d8
qiyisi <qiyisi@qq.com>
2021-06-11 15:40:03 +0800

    Update home

---
+++ packages/site/src/components/Link.js
@@ -0,0 +1,22 @@
+import styled, { css } from "styled-components";
+import { useSelector } from "react-redux";
+import { nodeSelector } from "store/reducers/nodeSlice";
+
+const Wrapper = styled.a`
+  color: #f22279;
+  cursor: pointer;
+  ${(p) =>
+    p.node === "kusama" &&
+    css`
+      color: #265deb;
+    `}
+`;
+
+export default function Link({ href, children }) {
+  const node = useSelector(nodeSelector);
+  return (
+    <Wrapper node={node} href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </Wrapper>
+  );
+}
--- packages/site/src/utils/index.js
+++ packages/site/src/utils/index.js
@@ -1,4 +1,5 @@
 export function addressEllipsis(address, start = 4, end = 4) {
   if (address.length <= start + end) return address;
+  if (!address.slice) return address;
   return `${address.slice(0, start)}...${address.slice(-end)}`;
 }

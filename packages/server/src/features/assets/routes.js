279fa9b7e4c390074530118bd2156b9904fecd6e
chaojun <chaojun@opensquare.network>
2021-06-15 16:44:03 +0800

    Add asset apis

--- packages/server/src/features/assets/routes.js
+++ packages/server/src/features/assets/routes.js
@@ -5,5 +5,18 @@ const router = new Router();
 
 router.get("/assets/latest", assetsController.getLatestAssets);
 router.get("/assets/count", assetsController.getAssetsCount);
+router.get("/assets", assetsController.getAssets);
+router.get(
+  "/assets/:assetId(\\d+)_:blockHeight(\\d+)",
+  assetsController.getAsset
+);
+router.get(
+  "/assets/:assetId(\\d+)_:blockHeight(\\d+)/transfers",
+  assetsController.getAssetTransfers
+);
+router.get(
+  "/assets/:assetId(\\d+)_:blockHeight(\\d+)/holders",
+  assetsController.getAssetHolders
+);
 
 module.exports = router;

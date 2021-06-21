95bdb6ad56b711ab7189473d532d0509a73ff96f
yoshiyuki <brother-zhou@qq.com>
2021-06-21 12:58:34 +0800

    refactor: componentlize Search-L

--- packages/site/src/components/Header/Subheader.js
+++ packages/site/src/components/Header/Subheader.js
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 import { useNode } from "utils/hooks";
+import SearchL from "../Search/search-l";
 
 const Wrapper = styled.div`
   display: flex;
@@ -38,149 +39,17 @@ const Title = styled.h1`
   }
 `;
 
-const ExploreWrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-
-  > :not(:first-child) {
-    margin-left: 32px;
-  }
-
-  @media screen and (max-width: 900px) {
-    flex-direction: column;
-    max-width: 318px;
-    width: 100%;
-    > * {
-      width: 100% !important;
-    }
-
-    > :not(:first-child) {
-      margin: 16px 0 0;
-    }
-  }
-`;
-
-const ExploreInput = styled.input`
-  width: 480px;
-  padding: 12px 16px;
-  background: #f4f4f4;
-  border-radius: 8px;
-  font-size: 16px;
-  line-height: 20px;
-  outline: none;
-  border: none;
-
-  ::placeholder {
-    color: rgba(17, 17, 17, 0.35);
-  }
-
-  :active,
-  :focus {
-    background-color: #ffffff;
-  }
-`;
-
-const ExploreButton = styled.div`
-  background: #f22279;
-  border-radius: 8px;
-  padding: 12px 16px;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 20px;
-  color: #ffffff;
-  text-align: center;
-  cursor: pointer;
-  ${(p) =>
-    p.node === "kusama" &&
-    css`
-      background: #000000;
-    `}
-`;
-
-const ExploreHintsWrapper = styled.div`
-  // todo make a better scroll bar
-  //overflow-y: scroll;
-  margin-left: 0 !important;
-  top: 53px;
-  left: 0;
-  width: 480px;
-  max-height: 308px;
-  position: absolute;
-`;
-
-const ExploreHint = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  margin: 0;
-  padding: 0 16px 0 16px;
-  line-height: 48px;
-  background-color: #ffffff;
-  font-size: 15px;
-
-  svg,
-  img {
-    margin-right: 8px;
-  }
-
-  * {
-    margin: 0;
-  }
-
-  :hover {
-    background-color: #fafafa;
-  }
-`;
-
-const Token = styled.span`
-  margin-right: 8px;
-  overflow: hidden;
-  width: 48px;
-  font-weight: 500;
-  //font-size: 15px;
-  color: #111111;
-`;
-
-const TokenDesc = styled.span`
-  width: 299px;
-  margin-right: 8px;
-  overflow: hidden;
-  color: rgba(17, 17, 17, 0.35);
-`;
-
-const Height = styled.span`
-  color: rgba(17, 17, 17, 0.65);
-`;
-
 export default function Subheader() {
   const node = useNode();
   const hints = [1, 2, 3, 4, 5];
+  const logo = `/imgs/logo-img-${
+    node === "kusama" ? "kusama.svg" : "polkadot.svg"
+  }`;
   return (
     <Wrapper>
-      <Logo
-        src={
-          node === "kusama"
-            ? "/imgs/logo-img-kusama.svg"
-            : "/imgs/logo-img-polkadot.svg"
-        }
-        alt="logo"
-      />
+      <Logo src={logo} alt="logo" />
       <Title>Statemint Explorer</Title>
-      <ExploreWrapper>
-        <ExploreInput placeholder="Address / Transaction / Asset..." />
-        <ExploreButton node={node}>Explore</ExploreButton>
-        <ExploreHintsWrapper>
-          {hints.map((hint) => (
-            <ExploreHint>
-              <img src="/imgs/token-icons/osn.svg" alt="icon" />
-              <Token>OSN</Token>
-              <TokenDesc>OpenSquare</TokenDesc>
-              <Height>#12450</Height>
-            </ExploreHint>
-          ))}
-        </ExploreHintsWrapper>
-      </ExploreWrapper>
+      <SearchL hints={hints} node={node} />
     </Wrapper>
   );
 }

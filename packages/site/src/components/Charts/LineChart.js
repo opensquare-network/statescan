def2692bef42709d7a29aaf4b1d71f4b0f43c2ca
yoshiyuki <brother-zhou@qq.com>
2021-06-22 10:57:57 +0800

    feature: Refs #21, line chart

--- packages/site/src/components/Charts/LineChart.js
+++ packages/site/src/components/Charts/LineChart.js
@@ -5,23 +5,8 @@ export default function LineChart({ data = [], color = "#F22279" }) {
     <Chart padding={[0, 0, 0, 0]} width={227} height={48} data={data}>
       <Axis name="time" visible={false} />
       <Axis name="price" visible={false} />
-      <LineAdvance
-        shape="smooth"
-        area
-        position="time*price"
-        color={color}
-        tooltip={[
-          "time*price",
-          (time, price) => {
-            return {
-              title: "2021-06-22",
-              name: "Price",
-              value: `$${price}`,
-            };
-          },
-        ]}
-      />
-      <Tooltip />
+      <LineAdvance shape="smooth" area position="time*price" color={color} />
+      <Tooltip custom={true} containerTpl={``} />
     </Chart>
   );
 }

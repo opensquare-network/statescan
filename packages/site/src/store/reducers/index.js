064810eed922ee0acc56b81c6f7c6f323c506138
qiyisi <qiyisi@qq.com>
2021-06-24 11:49:51 +0800

    Add toast

--- packages/site/src/store/reducers/index.js
+++ packages/site/src/store/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import nodeReducer from "./nodeSlice";
 import chainReducer from "./chainSlice";
+import toastReducer from "./toastSlice";
 
 export default combineReducers({
   node: nodeReducer,
   chain: chainReducer,
+  toast: toastReducer,
 });

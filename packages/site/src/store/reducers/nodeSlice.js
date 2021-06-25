d6524dc5833b9b8cf17fdeaaa130449ab3f43212
Yongfeng LI <wliyongfeng@gmail.com>
2021-06-25 15:24:18 +0800

    Improve the process to set node

--- packages/site/src/store/reducers/nodeSlice.js
+++ packages/site/src/store/reducers/nodeSlice.js
@@ -17,15 +17,12 @@ const nodeSlice = createSlice({
 
 export const { setNode } = nodeSlice.actions;
 
-export const nodeSelector = (state) => {
-  if (state.node.node) {
-    return state.node.node;
-  } else {
-    const storedNode = window.localStorage.getItem("node");
-    const node = storedNode ? JSON.parse(storedNode) : DEFAULT_NODE;
-    setNode(node);
-    return node;
-  }
+export const setStoredOrDefaultNode = () => (dispatch) => {
+  const storedNode = window.localStorage.getItem("node");
+  const node = storedNode ? JSON.parse(storedNode) : DEFAULT_NODE;
+  dispatch(setNode(node));
 };
+
+export const nodeSelector = (state) => state.node.node;
 
 export default nodeSlice.reducer;

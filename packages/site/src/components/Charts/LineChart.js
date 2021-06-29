d92d4ab61a09b2fbb4122453ec99126853957610
yoshiyuki <brother-zhou@qq.com>
2021-06-29 16:04:10 +0800

    feature: refs#167 , chart default styles

--- packages/site/src/components/Charts/LineChart.js
+++ packages/site/src/components/Charts/LineChart.js
@@ -1,13 +1,19 @@
 import { Axis, Chart, LineAdvance, Tooltip } from "bizcharts";
 import styled from "styled-components";
 
-const ChartWrapper = styled.div``;
+const ChartWrapper = styled.div`
+  svg,
+  img {
+    margin-top: 17px;
+  }
+`;
 
 const Title = styled.h2`
   margin: 0;
   font-size: 12px;
   text-align: right;
   color: rgba(17, 17, 17, 0.2);
+  font-weight: 400;
 `;
 
 export default function LineChart({
@@ -18,12 +24,21 @@ export default function LineChart({
   return (
     <ChartWrapper>
       <Title>{token} · Last 30d</Title>
-      <Chart padding={[0, 0, 0, 0]} width={227} height={34} data={data}>
-        <Axis name="time" visible={false} />
-        <Axis name="price" visible={false} />
-        <LineAdvance shape="smooth" area position="time*price" color={color} />
-        <Tooltip custom={true} containerTpl={`<i></i>`} />
-      </Chart>
+      {data.length ? (
+        <Chart padding={[0, 0, 0, 0]} width={227} height={34} data={data}>
+          <Axis name="time" visible={false} />
+          <Axis name="price" visible={false} />
+          <LineAdvance
+            shape="smooth"
+            area
+            position="time*price"
+            color={color}
+          />
+          <Tooltip custom={true} containerTpl={`<i></i>`} />
+        </Chart>
+      ) : (
+        <img src="/imgs/nochart.svg" alt="NoChartDataLoaded" />
+      )}
     </ChartWrapper>
   );
 }

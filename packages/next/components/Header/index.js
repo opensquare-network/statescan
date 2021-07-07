1e07889e38458730404e7344e0587eb6d2f5231f
qiyisi <qiyisi@qq.com>
2021-07-07 16:42:26 +0800

    Update

---
+++ packages/next/components/Header/index.js
@@ -0,0 +1,159 @@
+import { useState, useRef, useEffect } from "react";
+import styled, { css } from "styled-components";
+
+const Container = styled.header`
+  position: relative;
+  padding: 0 2rem;
+  @media screen and (max-width: 1200px) {
+    padding: 0 1.5rem;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 68px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  z-index: -1;
+  height: 320px;
+  background: linear-gradient(180deg, #eeeeee 0%, #ffffff 100%);
+  ${(p) =>
+    p.isHomePage &&
+    css`
+      height: 464px;
+    `};
+`;
+
+const Masked = styled.div`
+  width: 100%;
+  height: 100%;
+  opacity: 0.8;
+  position: relative;
+  top: -8px;
+  background: radial-gradient(
+    39.66% 101.89% at 50.29% 24.73%,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(221, 221, 221, 0.32) 100%
+  );
+  -webkit-mask-image: url("/imgs/pattern-dot.svg");
+  mask-image: url("/imgs/pattern-dot.svg");
+  -webkit-mask-repeat: repeat;
+  mask-repeat: repeat;
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  display: none;
+  width: 36px;
+  height: 36px;
+  background: #f4f4f4;
+  border-radius: 8px;
+  margin-right: 12px;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  padding: 6px;
+  > svg {
+    stroke: rgba(17, 17, 17, 0.65);
+  }
+  :hover {
+    background: #fafafa;
+    > svg {
+      stroke: #111111;
+    }
+  }
+  ${(p) =>
+    p.isActive &&
+    css`
+      background: #fafafa;
+      > svg {
+        stroke: #111111;
+      }
+    `}
+  @media screen and (max-width: 900px) {
+    display: block;
+  }
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  margin-left: 64px;
+  @media screen and (max-width: 900px) {
+    position: absolute;
+    left: 24px;
+    top: 60px;
+    margin: 0;
+    background: #ffffff;
+    box-shadow: 0px 6px 25px rgba(0, 0, 0, 0.04),
+      0px 1.80882px 5.94747px rgba(0, 0, 0, 0.0260636),
+      0px 0.751293px 0.932578px rgba(0, 0, 0, 0.02),
+      0px 0.271728px 0px rgba(0, 0, 0, 0.0139364);
+    border-radius: 8px;
+    padding: 8px 0px;
+    flex-direction: column;
+    width: 80px;
+  }
+`;
+
+// const MenuItem = styled(Link)`
+//   font-weight: 500;
+//   font-size: 15px;
+//   line-height: 20px;
+//   cursor: pointer;
+//   text-decoration: none;
+//   color: #111111;
+//   :hover {
+//     color: #f22279;
+//   }
+//   :not(:first-child) {
+//     margin-left: 40px;
+//   }
+//   @media screen and (max-width: 900px) {
+//     padding: 6px 12px;
+//     :hover {
+//       color: inherit;
+//       background: #fafafa;
+//     }
+//     :not(:first-child) {
+//       margin-left: 0;
+//     }
+//     ${(p) =>
+//       p.selected &&
+//       css`
+//         background: #fafafa;
+//       `}
+//   }
+// `;
+
+export default function Header() {
+  return (
+    <div>
+      <Container>
+        <Background isHomePage={false}>
+          <Masked />
+        </Background>
+        <Wrapper>
+          <FlexWrapper>
+            <IconWrapper>{/* <IconActive /> */}</IconWrapper>
+            <img src="/imgs/logo.svg" alt="logo" />
+          </FlexWrapper>
+          <FlexWrapper>
+            {/* <SearchS /> */}
+            {/* <NodeSwitcher /> */}
+          </FlexWrapper>
+        </Wrapper>
+      </Container>
+      {/* {isHomePage && <Subheader />} */}
+    </div>
+  );
+}

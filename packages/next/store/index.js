ef08b317a603992e60b376c439da1239161f96ee
chaojun <chaojun@opensquare.network>
2021-07-08 18:46:56 +0800

    Refs #233, add libs

--- packages/next/store/index.js
+++ packages/next/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 
-const store = configureStore({ reducer: rootReducer });
-export default store;
+export const store = configureStore({ reducer: rootReducer });

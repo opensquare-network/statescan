7a83bf62041f917b9b212baa773681e96cfe8cce
qiyisi <qiyisi@qq.com>
2021-07-09 14:53:57 +0800

    Update root

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -1,5 +1,23 @@
-export default function Index({}) {
-  return <div>root</div>;
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { useNode } from "utils/hooks";
+import { useDispatch } from "react-redux";
+import { setStoredOrDefaultNode } from "../store/reducers/nodeSlice";
+
+export default function Root() {
+  const router = useRouter();
+  const node = useNode();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!node) {
+      dispatch(setStoredOrDefaultNode());
+    } else {
+      router.push(`/${node}`);
+    }
+  }, [node, router, dispatch]);
+  return null;
 }
 
 export async function getServerSideProps(context) {

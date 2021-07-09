d679234230104093407fcf0d9b1a1b2bc83dd6f5
qiyisi <qiyisi@qq.com>
2021-07-09 10:47:42 +0800

    Add footer

--- packages/next/components/footer/index.js
+++ packages/next/components/footer/index.js
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+
+import Container from "components/container";
+import Producer from "./producer";
+import Donation from "./donation";
+
+const Wrapper = styled.div`
+  height: 84px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > * {
+    white-space: nowrap;
+  }
+  @media screen and (max-width: 900px) {
+    height: 116px;
+    flex-direction: column;
+    justify-content: center;
+    > :not(:first-child) {
+      margin-top: 12px;
+    }
+  }
+`;
+
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <Container>
+        <Wrapper>
+          <Producer />
+          <Donation />
+        </Wrapper>
+      </Container>
+    </footer>
+  );
 }

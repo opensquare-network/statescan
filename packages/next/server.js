581c9e06dc1a866642153a32dadd3ad801e8e754
chaojun <chaojun@opensquare.network>
2021-07-13 13:25:48 +0800

    Refs #261, merge server package into next

--- packages/next/server.js
+++ packages/next/server.js
@@ -1,73 +1,53 @@
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
-const httpProxy = require("http-proxy");
 const { loadEnvConfig } = require("@next/env");
 
 // Load next env
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
+const { initDb } = require("./backend/mongo");
+const { koaHandler, ioHandler } = require("./backend");
+
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextHandler = app.getRequestHandler();
 
 const PORT = process.env.PORT;
 if (!PORT) {
-  console.log("PROT is not defined");
+  console.log("PORT is not defined");
   process.exit();
 }
 
-const API_URL = process.env.API_URL;
-if (!API_URL) {
-  console.log("API_URL is not defined");
-  process.exit();
-}
+app
+  .prepare()
+  .then(() => initDb())
+  .then(() => {
+    const httpServer = createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { pathname } = parsedUrl;
 
-const proxy = httpProxy.createProxyServer(
-  API_URL === "https://dev-api.statescan.io/"
-    ? {
-        changeOrigin: true,
-        target: {
-          protocol: "https:",
-          host: "dev-api.statescan.io",
-          port: 443,
-        },
+      if (pathname.startsWith("/api/")) {
+        req.url = req.url.replace(/^\/api/, "");
+
+        koaHandler(req, res);
+      } else {
+        nextHandler(req, res, parsedUrl);
       }
-    : {
-        target: API_URL,
-      }
-);
+    });
 
-app.prepare().then(() => {
-  const httpServer = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname } = parsedUrl;
+    const io = require("socket.io")(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
 
-    if (pathname.startsWith("/api/")) {
-      req.url = req.url.replace(/^\/api/, "");
+    ioHandler(io);
 
-      proxy.web(req, res);
-    } else {
-      handle(req, res, parsedUrl);
-    }
+    httpServer.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
   });
-
-  httpServer.on("upgrade", function (req, socket, head) {
-    const parsedUrl = parse(req.url, true);
-    const { pathname } = parsedUrl;
-
-    if (pathname.startsWith("/api/")) {
-      req.url = req.url.replace(/^\/api/, "");
-
-      proxy.ws(req, socket, head);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  });
-
-  httpServer.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-});

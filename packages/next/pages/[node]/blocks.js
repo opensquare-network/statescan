44d5faeb0f2dc8dd91997cd0cb3ef33514e69488
chaojun <chaojun@opensquare.network>
2021-08-02 18:27:09 +0800

    fix

--- packages/next/pages/[node]/blocks.js
+++ packages/next/pages/[node]/blocks.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Layout from "components/layout";
 import { ssrNextApi as nextApi } from "services/nextApi";
 import { blocksHead, EmptyQuery } from "utils/constants";

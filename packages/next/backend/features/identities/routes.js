10b5166eab02a6485123bd4aa47d5295f15dda9f
chaojun <chaojun@opensquare.network>
2021-08-06 17:19:02 +0800

    Remove identity api

--- packages/next/backend/features/identities/routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router");
-const identitiesController = require("./identities.controller");
-
-const router = new Router();
-
-router.get("/identities/:address", identitiesController.getIdentity);
-
-module.exports = router;

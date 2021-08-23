53a85a783baf25180131923567071f34ab10b091
qiyisi <qiyisi@qq.com>
2021-08-23 11:14:33 +0800

    Init chartjs

---
+++ packages/next/components/analyticsChart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+      yAxisID: "y-axis-1",
+    },
+    {
+      label: "# of No Votes",
+      data: [1, 2, 1, 1, 2, 2],
+      fill: false,
+      backgroundColor: "rgb(54, 162, 235)",
+      borderColor: "rgba(54, 162, 235, 0.2)",
+      yAxisID: "y-axis-2",
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+    },
+    y1: {
+      type: "linear",
+      display: true,
+      position: "right",
+
+      // grid line settings
+      grid: {
+        drawOnChartArea: false, // only want the grid lines for one axis to show up
+      },
+    },
+  },
+};
+
+export default function AnalyticsChart() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Multi Axis Line Chart</h1>
+        <div className="links">
+          <a
+            className="btn btn-gh"
+            href="https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/MultiAxisLine.js"
+          >
+            Github Source
+          </a>
+        </div>
+      </div>
+      <Line data={data} options={options} />
+    </>
+  );
+}

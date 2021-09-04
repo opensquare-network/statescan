03bb9168df6900ab580f563a66c9f9ff40880bc1
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-04 14:53:38 +0800

    Refactor call normalization

--- packages/scan/src/extrinsic/index.js
+++ packages/scan/src/extrinsic/index.js
@@ -1,5 +1,5 @@
+const { normalizeCall } = require("../utils/normalize/call");
 const { extractExtrinsicEvents } = require("../utils");
-const { u8aToHex } = require("@polkadot/util");
 const {
   handleTeleportAssetDownwardMessage,
   handleTeleportAssets,
@@ -44,38 +44,6 @@ async function handleExtrinsic(extrinsic, indexer) {
       addAddress(indexer.blockHeight, signer);
     }
   }
-}
-
-function normalizeCall(call) {
-  const { section, method } = call;
-  const callIndex = u8aToHex(call.callIndex);
-
-  const args = [];
-  for (let index = 0; index < call.args.length; index++) {
-    let arg = call.args[index];
-
-    const argMeta = call.meta.args[index];
-    const name = argMeta.name.toString();
-    const type = argMeta.type.toString();
-    if (type === "Call" || type === "CallOf") {
-      args.push([name, normalizeCall(arg)]);
-      continue;
-    }
-
-    if (type === "Vec<Call>" || type === "Vec<CallOf>") {
-      args.push([name, arg.map(normalizeCall)]);
-      continue;
-    }
-
-    args.push([name, arg.toHuman()]);
-  }
-
-  return {
-    callIndex,
-    section,
-    method,
-    args: Object.fromEntries(args),
-  };
 }
 
 module.exports = {

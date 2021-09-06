51425720a05e82b3f9554d4b64762a2aca6ebbdb
yoshiyuki <brother-zhou@qq.com>
2021-09-06 14:25:11 +0800

    WIP: show identity in json table

---
+++ packages/next/components/account/address.js
@@ -0,0 +1,63 @@
+import Identity from "./identity";
+import CopyText from "../copyText";
+import BreakText from "../breakText";
+import MinorText from "../minorText";
+import MonoText from "../monoText";
+import Source from "./source";
+import { useEffect, useState } from "react";
+import { nodes } from "../../utils/constants";
+import _ from "lodash";
+
+function Address({address}) {
+
+  const [identity, setIdentity] = useState(null);
+  const node = 'westmint';//todo dynamic this
+  const relayChain =
+    nodes.find((item) => item.value === node)?.sub?.toLowerCase() || "kusama";
+
+  useEffect(() => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_IDENTITY_SERVER_HOST}/${relayChain}/identity/${address}`
+    )
+      .then((res) => res.json())
+      .then(res => {
+        !_.isEmpty(res) && setIdentity(res);
+      })
+      .catch(() => null)
+  }, []);
+
+
+  if (!identity) {
+    return <CopyText text={address}>
+      <BreakText>
+        <MinorText>
+          <MonoText>{address}</MonoText>
+        </MinorText>
+      </BreakText>
+    </CopyText>
+  }
+
+  return (<div
+    style={{
+      display: "flex",
+      flexWrap: "wrap",
+      paddingTop: 8,
+      paddingBottom: 8,
+    }}
+  >
+    <Identity identity={identity}/>
+    <CopyText text={address}>
+      <BreakText>
+        <MinorText>
+          <MonoText>{address}</MonoText>
+        </MinorText>
+      </BreakText>
+    </CopyText>
+    <Source
+      relayChain={relayChain}
+      address={address}
+    />
+  </div>)
+}
+
+export default Address;
--- packages/next/components/table/innerDataTable.js
+++ packages/next/components/table/innerDataTable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import BreakText from "components/breakText";
+import Address from "../account/address";
 
 const StyledTable = styled.table`
   width: 100%;
@@ -17,35 +18,39 @@ const StyledTable = styled.table`
 
 const StyledTr = styled.tr`
   ${(p) =>
-    p.nested
-      ? css`
-          :first-child {
-            > td {
-              border-width: 0 0 0 1px;
-              :first-child {
-                border-width: 0;
-              }
-            }
-          }
-          :not(:first-child) {
-            > td {
-              border-width: 1px 0 0 1px;
-              :first-child {
-                border-width: 1px 0 0 0;
-              }
-            }
-          }
-        `
-      : css`
-          :last-child {
-            > td {
-              border-width: 1px 0 1px 1px;
-              :last-child {
-                border-width: 1px 1px 1px 1px;
-              }
-            }
-          }
-        `}
+          p.nested
+                  ? css`
+                    :first-child {
+                      > td {
+                        border-width: 0 0 0 1px;
+
+                        :first-child {
+                          border-width: 0;
+                        }
+                      }
+                    }
+
+                    :not(:first-child) {
+                      > td {
+                        border-width: 1px 0 0 1px;
+
+                        :first-child {
+                          border-width: 1px 0 0 0;
+                        }
+                      }
+                    }
+                  `
+                  : css`
+                    :last-child {
+                      > td {
+                        border-width: 1px 0 1px 1px;
+
+                        :last-child {
+                          border-width: 1px 1px 1px 1px;
+                        }
+                      }
+                    }
+                  `}
 `;
 
 const StyledTd = styled.td`
@@ -65,47 +70,66 @@ const StyledTd = styled.td`
   }
 `;
 
-export default function InnerDataTable({ data, nested = false }) {
+export default function InnerDataTable({data, nested = false}) {
 
   if (React.isValidElement(data)) {
     return data;
   }
 
-  const formatValue = (fieldValue) =>
-    Array.isArray(fieldValue) ? (
-      <StyledTd style={{ padding: 0 }}>
-        <InnerDataTable data={fieldValue} nested />
-      </StyledTd>
-    ) : typeof fieldValue === "object" ? (
-      fieldValue === null ? (
-        <StyledTd style={{ minWidth: 320, padding: "10px 24px" }}>
-          null
+  const formatValue = (fieldValue, fieldName) => {
+    switch (typeof fieldValue) {
+
+      case "string":
+        switch (fieldName) {
+          case "Id":
+            return <StyledTd style={{minWidth: 320, padding: "10px 24px"}}>
+              <Address address={fieldValue}/>
+            </StyledTd>;
+          default:
+            return <StyledTd style={{minWidth: 320, padding: "10px 24px"}}>
+              <BreakText>{fieldValue.toString()}</BreakText>
+            </StyledTd>;
+        }
+
+      case "object":
+        switch (Array.isArray(fieldValue)) {
+          case true:
+            return <StyledTd style={{padding: 0}}>
+              <InnerDataTable data={fieldValue} nested/>
+            </StyledTd>
+          case false:
+            return (fieldValue === null ? (
+              <StyledTd style={{minWidth: 320, padding: "10px 24px"}}>
+                null
+              </StyledTd>
+            ) : React.isValidElement(fieldValue) ? (
+              <StyledTd style={{minWidth: 320, padding: "10px 24px"}}>
+                {fieldValue}
+              </StyledTd>
+            ) : (
+              <StyledTd style={{padding: 0}}>
+                <InnerDataTable data={fieldValue} nested/>
+              </StyledTd>
+            ));
+        }
+
+      default:
+        return <StyledTd style={{minWidth: 320, padding: "10px 24px"}}>
+          <BreakText>{fieldValue.toString()}</BreakText>
         </StyledTd>
-      ) : React.isValidElement(fieldValue) ? (
-        <StyledTd style={{ minWidth: 320, padding: "10px 24px" }}>
-          {fieldValue}
-        </StyledTd>
-      ) : (
-        <StyledTd style={{ padding: 0 }}>
-          <InnerDataTable data={fieldValue} nested />
-        </StyledTd>
-      )
-    ) : (
-      <StyledTd style={{ minWidth: 320, padding: "10px 24px" }}>
-        <BreakText>{fieldValue.toString()}</BreakText>
-      </StyledTd>
-    );
+    }
+  }
 
   if (Array.isArray(data) && data.length < 2) {
     return (
       data.length > 0 && (
         <StyledTable>
           <tbody>
-            {data.map((item, index) => (
-              <StyledTr key={index} nested={nested}>
-                {formatValue(item)}
-              </StyledTr>
-            ))}
+          {data.map((item, index) => (
+            <StyledTr key={index} nested={nested}>
+              {formatValue(item)}
+            </StyledTr>
+          ))}
           </tbody>
         </StyledTable>
       )
@@ -126,23 +150,23 @@ export default function InnerDataTable({ data, nested = false }) {
       entries.length > 0 && (
         <StyledTable>
           <tbody>
-            {entries.map(([fieldName, fieldValue], index) => {
-              return (
-                <StyledTr key={index} nested={nested}>
-                  <StyledTd
-                    style={{
-                      whiteSpace: "nowrap",
-                      width,
-                      minWidth: width,
-                      padding: "10px 24px",
-                    }}
-                  >
-                    {fieldName}
-                  </StyledTd>
-                  {formatValue(fieldValue)}
-                </StyledTr>
-              );
-            })}
+          {entries.map(([fieldName, fieldValue], index) => {
+            return (
+              <StyledTr key={index} nested={nested}>
+                <StyledTd
+                  style={{
+                    whiteSpace: "nowrap",
+                    width,
+                    minWidth: width,
+                    padding: "10px 24px",
+                  }}
+                >
+                  {fieldName}
+                </StyledTd>
+                {formatValue(fieldValue, fieldName)}
+              </StyledTr>
+            );
+          })}
           </tbody>
         </StyledTable>
       )

6ad792af6107667fd219ca09fe9012423d96f3fe
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-14 19:57:15 +0800

    Refactor AssetsEvents

--- packages/scan/src/event/assets/index.js
+++ packages/scan/src/event/assets/index.js
@@ -7,39 +7,10 @@ const {
 } = require("../../mongo");
 const { getApi } = require("../../api");
 const asyncLocalStorage = require("../../asynclocalstorage");
+const { Modules, AssetsEvents } = require("../../utils/constants");
 const { addAddresses } = require("../../store/blockAddresses");
 const { addAddress } = require("../../store/blockAddresses");
 const { toDecimal128 } = require("../../utils");
-
-const Modules = Object.freeze({
-  Assets: "assets",
-});
-
-const AssetsEvents = Object.freeze({
-  // Asset state
-  Created: "Created",
-  MetadataSet: "MetadataSet",
-  MetadataCleared: "MetadataCleared",
-  ForceCreated: "ForceCreated",
-  AssetStatusChanged: "AssetStatusChanged",
-  TeamChanged: "TeamChanged",
-  OwnerChanged: "OwnerChanged",
-  AssetFrozen: "AssetFrozen",
-  AssetThawed: "AssetThawed",
-  Destroyed: "Destroyed",
-
-  // Account
-  Transferred: "Transferred",
-  Frozen: "Frozen",
-  Thawed: "Thawed",
-  ApprovedTransfer: "ApprovedTransfer",
-  ApprovalCancelled: "ApprovalCancelled",
-  TransferredApproved: "TransferredApproved",
-
-  // Asset & Account
-  Issued: "Issued",
-  Burned: "Burned",
-});
 
 async function saveNewAssetTransfer(
   blockIndexer,
--- packages/scan/src/utils/constants.js
+++ packages/scan/src/utils/constants.js
@@ -1,6 +1,7 @@
 const Modules = Object.freeze({
   System: "system",
   Balances: "balances",
+  Assets: "assets",
 });
 
 const SystemEvents = Object.freeze({
@@ -18,8 +19,35 @@ const BalancesEvents = Object.freeze({
   BalanceSet: "BalanceSet",
 });
 
+const AssetsEvents = Object.freeze({
+  // Asset state
+  Created: "Created",
+  MetadataSet: "MetadataSet",
+  MetadataCleared: "MetadataCleared",
+  ForceCreated: "ForceCreated",
+  AssetStatusChanged: "AssetStatusChanged",
+  TeamChanged: "TeamChanged",
+  OwnerChanged: "OwnerChanged",
+  AssetFrozen: "AssetFrozen",
+  AssetThawed: "AssetThawed",
+  Destroyed: "Destroyed",
+
+  // Account
+  Transferred: "Transferred",
+  Frozen: "Frozen",
+  Thawed: "Thawed",
+  ApprovedTransfer: "ApprovedTransfer",
+  ApprovalCancelled: "ApprovalCancelled",
+  TransferredApproved: "TransferredApproved",
+
+  // Asset & Account
+  Issued: "Issued",
+  Burned: "Burned",
+});
+
 module.exports = {
   Modules,
   SystemEvents,
   BalancesEvents,
+  AssetsEvents,
 };

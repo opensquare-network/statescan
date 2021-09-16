9700fe1526c530828859a3cd37e1374c03970c82
yoshiyuki <brother-zhou@qq.com>
2021-09-16 17:02:26 +0800

    fix: show plain address when no identity (NO_ID)

--- packages/next/components/account/identity.js
+++ packages/next/components/account/identity.js
@@ -29,7 +29,7 @@ const Display = styled.span`
 `;
 
 export default function Identity({ identity }) {
-  if (!identity || identity?.status === 'NO_ID') {
+  if (!identity || identity?.info?.status === 'NO_ID') {
     return null;
   }
 

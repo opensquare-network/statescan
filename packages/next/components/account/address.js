95717bb928775dab88e058e13db077aca10e978f
yoshiyuki <brother-zhou@qq.com>
2021-09-23 09:37:20 +0800

    update: use a new identity api (short-ids)

--- packages/next/components/account/address.js
+++ packages/next/components/account/address.js
@@ -13,14 +13,22 @@ function Address({ address }) {
   const node = process.env.NEXT_PUBLIC_CHAIN;
   const relayChain =
     nodes.find((item) => item.value === node)?.sub?.toLowerCase() || "kusama";
-
+  const headers = {
+    accept: "application/json, text/plain, */*",
+    "content-type": "application/json;charset=UTF-8",
+  };
   useEffect(() => {
     fetch(
-      `${process.env.NEXT_PUBLIC_IDENTITY_SERVER_HOST}/${relayChain}/identity/${address}`
+      `${process.env.NEXT_PUBLIC_IDENTITY_SERVER_HOST}/${relayChain}/short-ids`,
+      {
+        headers,
+        method: "POST",
+        body: JSON.stringify({ addresses: [address] }),
+      }
     )
       .then((res) => res.json())
       .then((res) => {
-        !_.isEmpty(res) && setIdentity(res);
+        !_.isEmpty(res) && setIdentity(res[0]);
       })
       .catch(() => null);
   }, []);

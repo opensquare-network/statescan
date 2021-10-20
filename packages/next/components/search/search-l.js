be33627281f131af77d18712b4c1d9993021ab27
qiyisi <qiyisi@qq.com>
2021-10-20 17:51:23 +0800

    Update search hints

--- packages/next/components/search/search-l.js
+++ packages/next/components/search/search-l.js
@@ -3,7 +3,6 @@ import { useState, useCallback } from "react";
 import { useRouter } from "next/router";
 import { addToast } from "../../store/reducers/toastSlice";
 import { useDispatch } from "react-redux";
-import InLink from "components/inLink";
 import nextApi from "services/nextApi";
 import debounce from "lodash/debounce";
 import { useTheme } from "utils/hooks";
@@ -74,72 +73,6 @@ const SearchWrapper = styled.div`
   width: 480px;
 `;
 
-const ExploreHintsWrapper = styled.div`
-  z-index: 9999999;
-  background-color: #ffffff;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  margin-left: 0 !important;
-  top: 53px;
-  left: 0;
-  width: 480px;
-  max-height: 308px;
-  position: absolute;
-  border-radius: 8px;
-  border: 1px solid #f8f8f8;
-  box-shadow: 0px 6px 25px rgba(0, 0, 0, 0.04),
-    0px 1.80882px 5.94747px rgba(0, 0, 0, 0.0260636),
-    0px 0.751293px 0.932578px rgba(0, 0, 0, 0.02),
-    0px 0.271728px 0px rgba(0, 0, 0, 0.0139364);
-
-  .selected {
-    background-color: #fafafa;
-  }
-`;
-
-const ExploreHint = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  margin: 0;
-  padding: 0 16px 0 16px;
-  cursor: pointer;
-  line-height: 48px;
-  background-color: #ffffff;
-  font-size: 15px;
-
-  svg,
-  img {
-    margin-right: 8px;
-  }
-
-  * {
-    margin: 0;
-  }
-
-  :hover {
-    background-color: #fafafa;
-  }
-`;
-
-const Token = styled.span`
-  margin-right: 8px;
-  white-space: nowrap;
-  font-weight: 500;
-  color: #111111;
-`;
-
-const TokenDesc = styled.span`
-  width: 299px;
-  margin-right: 8px;
-  overflow: hidden;
-  color: rgba(17, 17, 17, 0.35);
-`;
-
-const Height = styled.span`
-  color: rgba(17, 17, 17, 0.65);
-`;
-
 export default function SearchL({ node }) {
   const dispatch = useDispatch();
   const router = useRouter();
@@ -147,15 +80,13 @@ export default function SearchL({ node }) {
   const [hints, setHints] = useState([]);
   const [focus, setFocus] = useState(false);
   const [selected, select] = useState(0);
-  const iconMap = new Map([["osn", "osn"]]);
   const theme = useTheme();
 
   const delayedQuery = useCallback(() => {
     return debounce((value) => {
-      console.log({ value });
       nextApi.fetch(`search/autocomplete?prefix=${value}`).then((res) => {
         console.log({ res });
-        setHints(res.result?.assets || []);
+        setHints(res?.result);
       });
     }, 500);
   }, []);
@@ -189,6 +120,7 @@ export default function SearchL({ node }) {
   };
 
   const onKeyDown = (e) => {
+    return;
     if (!focus) {
       return;
     }
@@ -232,26 +164,6 @@ export default function SearchL({ node }) {
       >
         Explore
       </ExploreButton>
-      {/* {focus && assets?.length > 0 && (
-        <ExploreHintsWrapper>
-          {assets.map((hint, index) => {
-            const icon = iconMap.get(hint.symbol.toLowerCase()) ?? "unknown";
-            return (
-              <InLink
-                key={index}
-                to={`/asset/${hint.assetId}_${hint.createdAt?.blockHeight}`}
-              >
-                <ExploreHint className={selected === index && "selected"}>
-                  <img src={`/imgs/token-icons/${icon}.svg`} alt="" />
-                  <Token>{hint.symbol}</Token>
-                  <TokenDesc>{hint.name}</TokenDesc>
-                  <Height>#{hint.assetId}</Height>
-                </ExploreHint>
-              </InLink>
-            );
-          })}
-        </ExploreHintsWrapper>
-      )} */}
     </ExploreWrapper>
   );
 }
--- packages/next/components/search/searchHints.js
+++ packages/next/components/search/searchHints.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Link from "next/link";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -12,12 +13,123 @@ const Wrapper = styled.div`
     0px 0.751293px 0.932578px rgba(0, 0, 0, 0.02),
     0px 0.271728px 0px rgba(0, 0, 0, 0.0139364);
   border-radius: 8px;
-  height: 200px;
   z-index: 99;
+  overflow-y: auto;
+`;
+
+const Title = styled.div`
+  padding: 8px 16px;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  color: rgba(17, 17, 17, 0.35);
+`;
+
+const BlockItem = styled.div`
+  min-height: 48px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  cursor: pointer;
+  :hover {
+    background-color: #fafafa;
+  }
+`;
+
+const BlockWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 20px;
+  > img {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+  }
+`;
+
+const AssetItem = styled.div`
+  min-height: 48px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  cursor: pointer;
+  :hover {
+    background-color: #fafafa;
+  }
+`;
+
+const AssetWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 20px;
+  flex-basis: 112px;
+  > img {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+  }
+`;
+
+const AssetName = styled.div`
+  font-size: 14px;
+  line-height: 20px;
+  color: rgba(17, 17, 17, 0.35);
+`;
+
+const IndexWrapper = styled.div`
+  font-size: 14px;
+  line-height: 20px;
+  color: rgba(17, 17, 17, 0.65);
+  margin-left: auto;
 `;
 
 export default function SearchHints({ hints, focus }) {
   if (!focus) return null;
+  if (!hints || (hints.assets?.length === 0 && hints.blocks?.length === 0))
+    return null;
 
-  return <Wrapper>search hints</Wrapper>;
+  return (
+    <Wrapper>
+      {hints.blocks?.length > 0 && (
+        <>
+          <Title>BLOCKS</Title>
+          {hints.blocks.map((item, index) => (
+            <Link href={`/block/${item.header?.number}`} key={index} passHref>
+              <BlockItem>
+                <BlockWrapper>
+                  <img src="/imgs/icons/latest-blocks.svg" alt="" />
+                  <div>Block</div>
+                </BlockWrapper>
+                <IndexWrapper>{`#${item.header?.number}`}</IndexWrapper>
+              </BlockItem>
+            </Link>
+          ))}
+        </>
+      )}
+      {hints.assets?.length > 0 && (
+        <>
+          <Title>ASSETS</Title>
+          {hints.assets.map((item, index) => (
+            <Link href={`/asset/${item.assetId}`} key={index} passHref>
+              <AssetItem>
+                <AssetWrapper>
+                  <img src="/imgs/token-icons/unknown.svg" alt="" />
+                  <div>{item.symbol}</div>
+                </AssetWrapper>
+                <AssetName>{item.name}</AssetName>
+                <IndexWrapper>{`#${item.assetId}`}</IndexWrapper>
+              </AssetItem>
+            </Link>
+          ))}
+        </>
+      )}
+    </Wrapper>
+  );
 }

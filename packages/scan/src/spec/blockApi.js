d448d62c37e7717a0ae12d7e0122e384bb894245
Yongfeng LI <wliyongfeng@gmail.com>
2021-11-01 11:39:18 +0800

    Remove useless stored block api

--- packages/scan/src/spec/blockApi.js
+++ packages/scan/src/spec/blockApi.js
@@ -18,6 +18,11 @@ async function findBlockApi(blockHash) {
   return blockApi;
 }
 
+function removeBlockApi(blockHash) {
+  delete blockApiMap[blockHash];
+}
+
 module.exports = {
   findBlockApi,
+  removeBlockApi,
 };

3718dd7aaca0cb105f3bea3144455f278f5ae0b2
qiyisi <qiyisi@qq.com>
2021-11-01 10:58:57 +0800

    Add asset 20 detail, #659

--- packages/next/utils/assetInfoData.js
+++ packages/next/utils/assetInfoData.js
@@ -50,6 +50,21 @@ const statemineInfo = new Map([
       ],
     },
   ],
+  [
+    20,
+    {
+      icon: "/imgs/icons/asset/bfkk.png",
+      about:
+        "A group of hobbyists who live in Berlin and we like the feelings of the nature.",
+      links: [
+        {
+          name: "Twitter: @sheenhuxin",
+          url: "https://twitter.com/sheenhuxin",
+          icon: "/imgs/icons/link-twitter.svg",
+        },
+      ],
+    },
+  ],
 ]);
 
 // node => assets info

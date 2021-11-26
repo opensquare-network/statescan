a023bf4d8fd568f2eb49044293b4a42c827bd850
Yongfeng LI <wliyongfeng@gmail.com>
2021-11-26 15:07:37 +0800

    Save nft known heights

---
+++ packages/nft-scan/src/mongo/service/known.js
@@ -0,0 +1,35 @@
+const { getScanStep } = require("../../env");
+const { getHeightCollection } = require("../knownHeight");
+
+async function getNextKnownHeights(beginHeight) {
+  const step = getScanStep();
+  const col = await getHeightCollection();
+  const records = await col
+    .find({
+      height: { $gte: beginHeight },
+    })
+    .sort({ height: 1 })
+    .limit(step)
+    .toArray();
+
+  return (records || []).map((item) => item.height);
+}
+
+async function saveKnownHeights(heights = []) {
+  if (heights.length <= 0) {
+    return;
+  }
+
+  const col = await getHeightCollection();
+  const bulk = col.initializeUnorderedBulkOp();
+  for (const height of heights) {
+    bulk.find({ height }).upsert().updateOne({ $set: { height } });
+  }
+
+  await bulk.execute();
+}
+
+module.exports = {
+  saveKnownHeights,
+  getNextKnownHeights,
+};
---
+++ packages/nft-scan/src/saveKnown.js
@@ -0,0 +1,59 @@
+const { closeDataDbClient } = require("./mongo");
+const { closeKnownClient } = require("./mongo/knownHeight");
+const { getNftTransferCollection } = require("./mongo");
+const { getInstanceAttributeCollection } = require("./mongo");
+const { getInstanceTimelineCollection } = require("./mongo");
+const { getInstanceCollection } = require("./mongo");
+const { getClassAttributeCollection } = require("./mongo");
+const { saveKnownHeights } = require("./mongo/service/known");
+const { getClassTimelineCollection } = require("./mongo");
+const { getClassCollection } = require("./mongo");
+require("dotenv").config();
+
+async function main() {
+  const heights = [];
+  const classCol = await getClassCollection();
+  const classes = await classCol.find({}).toArray();
+  heights.push(...classes.map((c) => c.indexer.blockHeight));
+
+  const classTimelineCol = await getClassTimelineCollection();
+  const timelineArr = await classTimelineCol.find({}).toArray();
+  heights.push(...timelineArr.map((t) => t.indexer.blockHeight));
+
+  let col = await getClassAttributeCollection();
+  let arr = await col.find({}).toArray();
+  heights.push(...arr.map((t) => t.indexer.blockHeight));
+
+  await saveKnownHeights(heights);
+  heights.splice(0);
+
+  col = await getInstanceCollection();
+  arr = await col.find({}).toArray();
+  heights.push(...arr.map((t) => t.indexer.blockHeight));
+
+  col = await getInstanceTimelineCollection();
+  arr = await col.find({}).toArray();
+  heights.push(...arr.map((t) => t.indexer.blockHeight));
+
+  col = await getInstanceAttributeCollection();
+  arr = await col.find({}).toArray();
+  heights.push(...arr.map((t) => t.indexer.blockHeight));
+
+  await saveKnownHeights(heights);
+  heights.splice(0);
+
+  col = await getNftTransferCollection();
+  arr = await col.find({}).toArray();
+  heights.push(...arr.map((t) => t.indexer.blockHeight));
+
+  await saveKnownHeights(heights);
+  heights.splice(0);
+}
+
+main()
+  .then(async () => {
+    console.log("Known heights saved");
+    await closeDataDbClient();
+    await closeKnownClient();
+  })
+  .catch(console.error);

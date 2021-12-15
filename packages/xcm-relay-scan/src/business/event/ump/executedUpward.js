7ec1bb67ccdc3c3e2f59f7f4cef294532fbf5db4
chaojun <chaojun@opensquare.network>
2021-12-15 13:46:40 +0800

    Refs #Save teleport out status

--- packages/xcm-relay-scan/src/business/event/ump/executedUpward.js
+++ packages/xcm-relay-scan/src/business/event/ump/executedUpward.js
@@ -1,4 +1,5 @@
-const { getExecutedCollection } = require("../../../mongo");
+const { getExecutedCollection, getUpwardMessageCollection } = require("../../../mongo");
+const { getTeleportOutCollection } = require("../../../mongo/parachain");
 
 async function handleExecutedUpward(event, indexer) {
   const [messageId, outcome] = event.data.toJSON();
@@ -9,6 +10,28 @@ async function handleExecutedUpward(event, indexer) {
     outcome,
     indexer,
   });
+
+  const teleportOutCol = await getTeleportOutCollection();
+  const upwardMessageCol = await getUpwardMessageCollection();
+  const upwardMessage = await upwardMessageCol.findOne({ msgId: messageId });
+  if (upwardMessage) {
+    const blockHash = upwardMessage.descriptor.paraHead;
+    const teleportOut = await teleportOutCol.findOne({ "indexer.blockHash": blockHash });
+    if (teleportOut) {
+      await teleportOutCol.updateOne(
+        { _id: teleportOut._id },
+        {
+          $set: {
+            relayChainInfo: {
+              enterAt:  upwardMessage.indexer,
+              executedAt: indexer,
+              outcome,
+            }
+          }
+        }
+      );
+    }
+  }
 }
 
 module.exports = {

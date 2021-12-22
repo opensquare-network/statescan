fc5a3321d13600b565f0ced630e047edc1892901
chaojun <chaojun@opensquare.network>
2021-12-22 16:06:54 +0800

    Paginated

--- packages/xcm-relay-scan/src/scripts/update-teleportout.js
+++ packages/xcm-relay-scan/src/scripts/update-teleportout.js
@@ -6,11 +6,33 @@ const { updateTeleportOutInfo } = require("../mongo/service");
 
 async function main() {
   const col = await getExecutedCollection();
-  const allExecuteds = await col.find({}).toArray();
-  for (const executed of allExecuteds) {
-    const { messageId, indexer, outcome } = executed;
-    console.log(`Updating ${messageId}:`, outcome);
-    await updateTeleportOutInfo(messageId, indexer, outcome);
+  let lastId = null;
+  while (true) {
+    const allExecuteds = await col
+      .find(
+        lastId
+        ? {
+            _id: {
+              $gt: lastId
+            }
+          }
+        : {}
+      )
+      .limit(100)
+      .sort({ _id: 1 })
+      .toArray();
+
+    if (allExecuteds.length === 0) {
+      break;
+    }
+
+    for (const executed of allExecuteds) {
+      const { messageId, indexer, outcome } = executed;
+      console.log(`Updating ${messageId}:`, outcome);
+      await updateTeleportOutInfo(messageId, indexer, outcome);
+    }
+
+    lastId = allExecuteds[allExecuteds.length - 1]._id;
   }
 }
 

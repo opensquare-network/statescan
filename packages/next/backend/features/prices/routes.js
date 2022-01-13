ba68b9a8021627c1f9c4c6bb16ead4cc1e35ce19
Yongfeng LI <wliyongfeng@gmail.com>
2022-01-13 12:20:46 +0800

    Fix statemint price api

--- packages/next/backend/features/prices/routes.js
+++ packages/next/backend/features/prices/routes.js
@@ -4,7 +4,7 @@ const pricesController = require("./prices.controller");
 const router = new Router();
 
 router.get(
-  "/:chain(westmint|statemine)/prices/daily",
+  "/:chain(westmint|statemine|statemint)/prices/daily",
   pricesController.getDailyPrices
 );
 

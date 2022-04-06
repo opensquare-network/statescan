4b874c9c8211f786c019d2139bc9397d35d0341f
Yongfeng LI <wliyongfeng@gmail.com>
2022-04-06 16:57:55 +0800

    Scan all instances of one class in the re-scan scripts

--- packages/nft-scan/src/scripts/rescan-ipfs.js
+++ packages/nft-scan/src/scripts/rescan-ipfs.js
@@ -35,6 +35,32 @@ async function scanInstance(classId, classHeight, instanceId, instanceHeight) {
   }
 }
 
+async function scanAllInstancesOfClass(classId, classHeight) {
+  console.log(`Re-scan all instances of class `, classId);
+
+  const query = { classId };
+  if (classHeight) {
+    query.classHeight = classHeight;
+  }
+
+  const instanceCol = await getInstanceCollection();
+  const nftInstances = await instanceCol
+    .find(query, {
+      sort: {
+        classHeight: -1,
+        "indexer.blockHeight": -1,
+      },
+    })
+    .toArray();
+
+  for (const instance of nftInstances) {
+    if (instance.dataHash) {
+      await scanMetadataAndSave(instance.dataHash, instance.metadata.data);
+      await handleImageByDataHash(instance.dataHash);
+    }
+  }
+}
+
 async function scanClass(classId, classHeight) {
   console.log(`Re-scan class data from IPFS for`, classId);
 
@@ -56,6 +82,8 @@ async function scanClass(classId, classHeight) {
     await scanMetadataAndSave(nftClass.dataHash, nftClass.metadata.data);
     await handleImageByDataHash(nftClass.dataHash);
   }
+
+  await scanMetadataAndSave(classId, classHeight);
 }
 
 async function main() {

5ae74b862e419dc536371b2534b54dac5e60107f
hyifeng <chaojun@opensquare.network>
2022-06-09 12:50:59 +0800

    Update nftclass timeline API (#1062)

--- packages/next/backend/features/nftclasses/routes.js
+++ packages/next/backend/features/nftclasses/routes.js
@@ -6,8 +6,16 @@ const router = new Router();
 router.get("/nft/classes", nftclassesController.getNftClasses);
 router.get("/nft/classes/:classId(\\d+)", nftclassesController.getNftClassById);
 router.get(
+  "/nft/classes/:classId(\\d+)/timeline",
+  nftclassesController.getNftClassTimelineById
+);
+router.get(
   "/nft/classes/:classId(\\d+)_:blockHeight(\\d+)",
   nftclassesController.getNftClass
+);
+router.get(
+  "/nft/classes/:classId(\\d+)_:blockHeight(\\d+)/timeline",
+  nftclassesController.getNftClassTimeline
 );
 
 module.exports = router;

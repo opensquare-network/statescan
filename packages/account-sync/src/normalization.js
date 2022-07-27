a15fab9e15b2627d70ba55a8c472015d9e4cca66
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-27 18:55:04 +0800

    Support known ss58 format

--- packages/account-sync/src/normalization.js
+++ packages/account-sync/src/normalization.js
@@ -3,6 +3,7 @@ const {
   env: { currentChain },
 } = require("@statescan/common");
 const { allNetworks } = require("@polkadot/networks");
+const isNil = require("lodash.isnil");
 
 function getRelayChainNetwork(chain) {
   if (chain === "statemine") {
@@ -13,13 +14,27 @@ function getRelayChainNetwork(chain) {
   return "polkadot";
 }
 
-function normalizeEntry([key, value]) {
+function getSs58Format() {
+  const chain = currentChain();
+  let possibleValue = {
+    litmus: 131,
+  }[chain];
+
+  if (!isNil(possibleValue)) {
+    return possibleValue;
+  }
+
   const relayChain = getRelayChainNetwork(currentChain());
   const targetNetwork = allNetworks.find(
     ({ network }) => network === relayChain
   );
+
+  return targetNetwork?.prefix || 42;
+}
+
+function normalizeEntry([key, value]) {
   const pubKeyU8a = key.slice(48);
-  const addr = encodeAddress(pubKeyU8a, targetNetwork?.prefix || 42);
+  const addr = encodeAddress(pubKeyU8a, getSs58Format());
   const detail = value.toJSON();
 
   return {
